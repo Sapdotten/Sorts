5ac1a643e7dd8631dadec3a6c9fddc4abe6dffdf
Sapdotten <semenova.a.p.04@gmail.com>
2023-12-06 12:33:23 +0400

    Add class for making random vector

--- include/stats.h
+++ include/stats.h
@@ -1,5 +1,7 @@
 #ifndef INCLUDE_STATS_H
 #define INCLUDE_STATS_H
+#include <vector>
+#include <ctime>
 namespace Sorts {
 
 struct Stats {
@@ -7,11 +9,22 @@ struct Stats {
     size_t copy_count = 0;
     Stats(int comp_count, int _copy_count)
         : comparison_count(comp_count), copy_count(_copy_count){};
-    Stats() = default;
+    Stats() : comparison_count(0), copy_count(0){};
     Stats& operator+(const Stats& other);
 
     Stats& operator+=(const Stats& other);
+    Stats& operator/(int num);
+    Stats& operator/=(int num);
   };
+
+class MyVector {
+    size_t seed;
+
+ public:
+    std::vector<int> GetRandomVector(int array_length);
+    MyVector() : seed(std::time(NULL)){};
+
+};
 
 }
 
--- src/sorts/stats.cc
+++ src/sorts/stats.cc
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_STATS_CC
 #define INCLUDE_STATS_CC
 #include "../include/stats.h"
+#include <random>
+#include <ctime>
+#include "../include/sorts.h"
+#include <iostream>
 namespace Sorts {
 
   Stats& Stats::operator+(const Stats& other) {
@@ -10,6 +14,24 @@ namespace Sorts {
   }
 
   Stats& Stats::operator+=(const Stats& other) { return this->operator+(other); }
+  Stats& Stats::operator/(int num){ 
+    comparison_count /= num;
+    copy_count /= num;
+    return *this;
+  };
+  Stats& Stats::operator/=(int num) { return this->operator/(num); };
+
+
+
+  std::vector<int> MyVector::GetRandomVector(const int array_length){
+    std::srand(seed);
+    std::vector<int> arr(array_length);
+    for (int i = 0; i < array_length; ++i) {
+      arr.at(i) = rand() % array_length;
+    }
+    return arr;
+  }
+  
 
   }  // namespace Sorts
 
